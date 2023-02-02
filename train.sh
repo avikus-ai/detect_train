005abab064ade39d2b79b9ae2e7484762f904d9a
Hwang Jung Hyun <junghyun.hwang@avikus.ai>
2023-02-02 16:36:37 +0900

    fix bug

--- train.sh
+++ train.sh
@@ -1,11 +1,12 @@
-python -m torch.distributed.run --nproc_per_node 3 train.py \
+python train.py \
 --data aiboat.yaml \
 --project AiBOAT_dev \
---name sahi \
---weights yolov5s.pt \
+--name random_crop \
+--weights yolov5n.pt \
 --img-size 960 \
---epochs 50 \
---batch-size 120 \
---device 1,2,3
+--epochs 2 \
+--batch-size 3 \
+--device 0 \
+--stop-coco-eval
 # python -m torch.distributed.run --nproc_per_node 2 train.py \
 # --cfg hub/yolov5m-transformer.yaml \
--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -806,7 +806,7 @@ class LoadImagesAndLabels(Dataset):
     def load_image(self,
                    i,
                    augment=False,
-                   random_crop=False):
+                   random_crop=True):
         # Loads 1 image from dataset index 'i', returns (im, original hw, resized hw)
         im, f, fn = self.ims[i], self.im_files[i], self.npy_files[i],
         if im is None:  # not cached in RAM
