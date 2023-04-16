af511266fa9c2d38a44f77db9de6d35ca0cfe6d1
youngjae-avikus <youngjae.you@avikus.ai>
2023-04-16 19:35:52 +0900

    Fix the train script according to the ir-fit and disable hsv augmentation

--- train_ir.sh
+++ train_ir.sh
@@ -1,11 +1,15 @@
-python3 train.py \
+#!/bin/bash
+
+python -m torch.distributed.run --nproc_per_node 2 \
+train.py \
 --project HiNAS-IR \
 --name v5l_1280_FLIR \
 --img_type ir \
+--hyp data/hyps/hyp-ir.yaml \
 --data data/data-ir.yaml \
 --cfg models/yolov5l-ir.yaml \
 --weights yolov5l.pt \
---device 0 \
+--device 0,1 \
 --imgsz 1280 \
 --epochs 100 \
---batch-size 5
+--batch-size 8
