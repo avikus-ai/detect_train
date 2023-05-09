79cc9a51e3df57ea688387f019b372186b0d1ed2
youngjae-avikus <youngjae.you@avikus.ai>
2023-05-09 09:40:10 +0900

    Update Seaspan-ir-xe3 export script

--- export_ir.sh
+++ export_ir.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 python export.py \
 	--data \
-    /data01/HiNAS-DATA/CV-MODEL/2023-IR-YOLOv5l/data-ir-avikus.yaml \
+    ./data/data-ir-avikus.yaml \
 	--weights \
-	/data01/HiNAS-DATA/CV-MODEL/2023-IR-YOLOv5l/b1_704_1280_ir_best.pt \
+	./Seaspan/IR/seaspan_ir.pt \
 	--include \
 	onnx \
 	--imgsz \
 	704 1280 \
 	--opset \
-	16 \
+	12 \
 	--batch-size \
 	1 \
 	--simplify \
