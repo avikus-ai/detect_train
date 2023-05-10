add58b734c91e4b8e43a60293afa8767460bd6f9
youngjae-avikus <youngjae.you@avikus.ai>
2023-05-10 11:16:12 +0900

    Update val.sh script

--- val_onnx.sh
+++ val_onnx.sh
@@ -4,7 +4,7 @@ python val.py \
     --data \
     data/eo-slicing-batch02.yaml \
     --weights \
-    Seaspan/EO/seaspan_eo.pt \
+    Seaspan/EO/seaspan_eo.onnx \
     --device \
     1 \
     --imgsz \
